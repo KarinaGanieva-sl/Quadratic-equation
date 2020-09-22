d950d8270b8928da2584e9c171a988775ff209bf
KarinaGanieva-sl <46797639+KarinaGanieva-sl@users.noreply.github.com>
2020-09-22 19:53:34 +0300

    Update quadratic_equation.c

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 #include "quadratic_equation.h"
 
-int isNull(double x)
+/**
+ * This method is used to check if double-value is close to zero.
+ * @param x double-value
+ * @return int returns 1 if true, 0 if false.
+ */
+int isCloseToZero(double x)
 {
+     assert (isfinite(x));
      if(fabs(x - 0.0) < EPS)
          return 1;
      return 0;
@@ -12,8 +19,10 @@ int isNull(double x)
 
 void getCoefficient(char message[], double *coef)
 {
+    assert (message != NULL);
+    assert (coef != NULL);
     printf("%s", message);
-    if(scanf("%lf", coef))
+    if(scanf("%lf", coef) && isfinite(*coef))
         return;
     printf(FAILURE_MESSAGE);
     exit(EXIT_FAILURE);
@@ -21,14 +30,23 @@ void getCoefficient(char message[], double *coef)
 
 double* solveQuadraticEquation(double a, double b, double c)
 {
-    double disc = b * b - 4 * a * c;
-    if(disc < 0 && !isNull(disc))
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    double disc = getDiscriminant(a,b,c);
+    if(!isfinite(disc))
+    {
+        printf(DISC_ERROR);
+        static double ans[1] = {0};
+        return ans;
+    }
+    if(disc < 0 && !isCloseToZero(disc))
     {
         printf(EMPTY_SET_ANSWER);
         static double ans[1] = {0};
         return ans;
     }
-    if(disc > 0 && !isNull(disc))
+    if(disc > 0 && !isCloseToZero(disc))
     {
         static double ans[3];
         ans[0] = 2;
@@ -44,9 +62,11 @@ double* solveQuadraticEquation(double a, double b, double c)
 
 double* solveLinearEquation(double b, double c)
 {
-    if(isNull(b))
+    assert (isfinite(b));
+    assert (isfinite(c));
+    if(isCloseToZero(b))
     {
-        if(isNull(c))
+        if(isCloseToZero(c))
             printf(R_ANSWER);
         else
             printf(EMPTY_SET_ANSWER);
@@ -61,7 +81,10 @@ double* solveLinearEquation(double b, double c)
 
 double* solve(double a, double b, double c)
 {
-    if(isNull(a))
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    if(isCloseToZero(a))
         return solveLinearEquation(b, c);
     else
         return solveQuadraticEquation(a, b, c);
@@ -69,9 +92,18 @@ double* solve(double a, double b, double c)
 
 void printAnswer(double* ans)
 {
-    printf("Answer: ");
+    assert (ans != NULL);
+    if(ans[0] == 0)
+        return;
     if(ans[0] == 1)
-        printf("x = %lf", ans[1]);
+        printf("Answer: x = %lf", ans[1]);
     else
-        printf("x1 = %lf, x2 = %lf", ans[1], ans[2]);
+        printf("Answer: x1 = %lf, x2 = %lf", ans[1], ans[2]);
+}
+
+double getDiscriminant(double a, double b, double c) {
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    return b * b - 4 * a * c;
 }
